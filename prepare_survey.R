a7a7ff55381bf31f2214cbe284053d1c84c4699d
courtiol <alexandre.courtiol@gmail.com>
2025-02-20 13:31:40 +0100

    Clean up and failed attempt to make total FTE dynamic

--- prepare_survey.R
+++ prepare_survey.R
@@ -65,8 +65,8 @@ name = "CSS")
 
 # First page of questionnaire ---------------------------------------------
 
-N0 <-  add_element(label = "# Study for MPA managers and staff:
-# Who's working on your marine protected area?	
+N0 <-  add_element(label = "# Study for Marine Protected Area managers and staff:
+# Who's working on your MPA?	
 
 <br>
 
@@ -95,7 +95,9 @@ S0 <- add_element(label = "Let's begin",
                   name = "S0",
                   type = "submit")
 
-P1 <-  add_element(label = "# <mark style='background-color:#6495ED;color:#FFD700'> Part A: Let's find your MPA </mark>", #FIXME? abreviation not yet defined
+# Second page of questionnaire ---------------------------------------------
+
+P1 <-  add_element(label = "# <mark style='background-color:#6495ED;color:#FFD700'> Part A: Let's find your Marine Protected Area (MPA)</mark>",
                    name = "P1",
                    type = "note")
 
@@ -103,7 +105,7 @@ N1 <-  add_element(label = "## 1: Select your country",
                    name = "N1",
                    type = "note")
 
-Q1 <- add_element(label = "#### Select the country (or overseas land) with the MPA(s) you are responding for
+Q1 <- add_element(label = "#### Select the country (or overseas land) with the MPA you are responding for
 💙 You can search using name, partial name, or ISO code.
 💡 Some overseas lands or islands are listed under their own name, e.g. Guadeloupe is listed under 'Guadeloupe (GLP)' and not under 'France (FRA)'.",
                   name = "country",
@@ -114,17 +116,19 @@ S1 <- add_element(label = "Continue",
                   name = "S1",
                   type = "submit")
 
-P2 <-  add_element(label = "# <mark style='background-color:#6495ED;color:#FFD700'> Part A: Let's find your MPA </mark>", #FIXME? abreviation not yet defined
+# Third page of questionnaire ---------------------------------------------
+
+P2 <-  add_element(label = "# <mark style='background-color:#6495ED;color:#FFD700'> Part A: Let's find your Marine Protected Area (MPA)</mark>",
                    name = "P2",
                    type = "note")
 
-N2 <-  add_element(label = "## 2: Select your Marine Protected Area (MPA)",
+N2 <-  add_element(label = "## 2: Select your MPA",
                    name = "N2",
                    type = "note")
 
 add_MPAs_country <- function(countrycode) {
   add_element(label = "#### Select the MPA you are responding for
-💙 You can search using name, partial name, or WDPA PID.
+💙 You can search using name, partial name, or World Database on Protected Areas (WDPA) PID.
 💡 The sites listed here are those present in WDPA for the country (or overseas land) you have selected above. If you cannot find your MPAs, please select 'MPA not listed'.",
               name = paste0("MPA_", countrycode, "_mc_multiple"),
               class = "cant_add_choice",
@@ -185,7 +189,6 @@ Q_issue1_text <- add_element(label = "#### 🛟 Tell us what is wrong:",
                   optional = "*",
                   showif = "Q_issue1")
 
-
 Warn_multiple <- add_element(label = r"(
 <div class='alert'>
 <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
@@ -200,6 +203,8 @@ Warn_multiple <- add_element(label = r"(
 S3 <- add_element(label = "Continue",
                   name = "S3",
                   type = "submit")
+
+# Fourth page of questionnaire ---------------------------------------------
 
 P4 <-  add_element(label = "# <mark style='background-color:#6495ED;color:#FFD700'> Part B: Tell us about your MPA (6 questions) </mark>", ##FIXME replace "your MPA" by its name
                    name = "P4",
@@ -221,40 +226,32 @@ M1 <- add_element(label = "#### Including yourself, what formal roles are involv
                  choice5 = "**Leadership Focused** — such as decision making",
                  choice6 = "**Other** — for roles unlisted above",
                  choice7 = "**None** — no one works on this MPA in a formal role",
-                 choice8 = "**I don't know**")
+                 choice8 = "**I don't know**",
+                 optional = "!")
 
 B1 <- add_element(label = "The answer **None** cannot be combined with another category",
                   name = "B1",
                   type = "block",
-                  showif = " (M1 %contains_word% '1' | M1 %contains_word% '2' | M1 %contains_word% '3' | M1 %contains_word% '4' | M1 %contains_word% '5' | M1 %contains_word% '6') && M1 %contains_word% '7'")
+                  showif = "(M1 %contains_word% '1' | M1 %contains_word% '2' | M1 %contains_word% '3' | M1 %contains_word% '4' | M1 %contains_word% '5' | M1 %contains_word% '6') && M1 %contains_word% '7'")
 
 B2 <- add_element(label = "The answer **I don't know** cannot be combined with another category",
                   name = "B2",
                   type = "block",
-                  showif = " (M1 %contains_word% '1' | M1 %contains_word% '2' | M1 %contains_word% '3' | M1 %contains_word% '4' | M1 %contains_word% '5' | M1 %contains_word% '6' | M1 %contains_word% '7') && M1 %contains_word% '8'")
+                  showif = "(M1 %contains_word% '1' | M1 %contains_word% '2' | M1 %contains_word% '3' | M1 %contains_word% '4' | M1 %contains_word% '5' | M1 %contains_word% '6' | M1 %contains_word% '7') && M1 %contains_word% '8'")
 
 S4 <- add_element(label = "Continue",
                   name = "S4",
                   type = "submit")
 
-# MCH1 <- add_element(label = "#### For each workforce category selected above, indicate the number of formal staff who work on this specific MPA
-# 💙 For each role, report the number of staff in that role or use COMMENT to record in your own way.
-# 💡 FTE = Full-time equivalent; Full-time all year; Part-time all year; Seasonal (e.g., summer), and Occasional (e.g., few weeks a year).
-# 🦀 Add a COMMENT to add more options or to explain anything you think we should know.",
-#                     type = "mc_heading",
-#                     #class = "mc_width120 rotate_label45",
-#                     name = "MCH1",
-#                     choice1 = "Full time all year\n (1 FTE)",
-#                     choice2 = "Part time all year\n (0.5 FTE)",
-#                     choice3 = "Seasonal\n (0.25 FTE)",
-#                     choice4 = "Occasional\n (0.1 FTE)")
+# Fifth page of questionnaire ---------------------------------------------
 
 P5 <-  add_element(label = "# <mark style='background-color:#6495ED;color:#FFD700'> Part B: Tell us about your MPA (6 questions) </mark>", ##FIXME replace "your MPA" by its name
                    name = "P5",
-                   type = "note")
+                   type = "note",
+                   showif = "!M1 %contains_word% '7' && !M1 %contains_word% '8'")
 
 N5 <-  add_element(label = "## 2: Number of people in each role. 
-#### For each workforce category selected previously, indicate the number of formal staff who work on this specific MPA
+#### For each workforce category selected previously, indicate the number of formal staff who work on this specific MPA.
 💙 For each role, report the number of staff in that role or use COMMENT to record in your own way.
 💡 FTE = Full-time equivalent.
 🦀 Add a COMMENT to add more options or to explain anything you think we should know.      
@@ -302,7 +299,7 @@ add_personel_questions <- function(label = "### **what**",
   bind_rows(E1, E2, E3, E4, E5, C1, C2)
 }
 
-## Those are personel questions, do make sure that this agrees with category numbers in M1 above!
+## Those are personnel questions, do make sure that this agrees with category numbers in M1 above!
 
 PERS1 <- add_personel_questions(label = "### **Site Focused**", category = "site",
                                 showif = "M1 %contains_word% '1'")
@@ -324,14 +321,17 @@ PERS6 <- add_personel_questions(label = "### **Other**", category = "other",
 
 S5 <- add_element(label = "Continue",
                   name = "S5",
-                  type = "submit")
+                  type = "submit",
+                  showif = "!M1 %contains_word% '7' && !M1 %contains_word% '8'")
+
+# Sixth page of questionnaire (optional) ---------------------------------------
 
 P6 <-  add_element(label = "# <mark style='background-color:#6495ED;color:#FFD700'> Part B: Tell us about your MPA (6 questions) </mark>", ##FIXME replace "your MPA" by its name
                    name = "P6",
                    type = "note")
 
 SUMM_note <- add_element(label = "## Confirm the number of FTE. 
-#### For each workforce category please review the number of FTE corresponding to your previous choices and adjust if necessary",
+#### For each workforce category please review the number of FTE corresponding to your previous choices and adjust if necessary.",
                          type = "note",
                          name = "SUMM_note")
 
@@ -370,9 +370,39 @@ FTE_other <- add_element(label = "### **Other**",
                          type = "number 0,999999,0.1",
                          value = FTE_formula("other"))
 
+FTE_total_compute <- add_element(label = r"(<script>
+var FTE_total = 0;
+$(document).ready(function () {
+    // for Select2, one must use the 'change' event
+    $('input[name^="FTE"]').on('change', function () {
+        var textread = $(this).val();
+        FTE_total = textread.reduce((sum, num) => sum + Number(num), 0).toString();;
+    });
+});
+</script>)",
+                                  name = "FTE_total_compute",
+                                  type = "note")
+
+FTE_total <- add_element(label = "### **TOTAL**
+<script>
+if (input) {
+  input.readOnly = true;
+}
+</script>",
+                         name = "FTE_total",
+                         type = "text",
+                         value = "FTE_total") ##FIXME compute sum and prevent modifications
+
+FTE_note <- add_element(label = "💡 The TOTAL is just the sum of the categories above; you cannot modify it directly.",
+                        name = "FTE_note",
+                        type = "note")
+
 S6 <- add_element(label = "Continue",
                   name = "S6",
                   type = "submit")
+
+# Seventh page of questionnaire (optional) ---------------------------------------
+
 
 # Save survey -----------------------------------------------------------
 
@@ -382,7 +412,8 @@ survey_tbl <- bind_rows(N0, S0,
                         P4, N4, M1, B1, B2, S4,
                         P5, N5,
                         PERS1, PERS2, PERS3, PERS4, PERS5, PERS6, S5,
-                        P6, SUMM_note, FTE_site, FTE_stakeholder, FTE_support, FTE_scientists, FTE_leadership, FTE_other, S6)
+                        P6, SUMM_note, FTE_site, FTE_stakeholder, FTE_support, FTE_scientists, FTE_leadership, FTE_other,
+                        FTE_total_compute, FTE_total, FTE_note, S6)
 
 if (!dir.exists("cleandata")) dir.create("cleandata")
 write.csv(survey_tbl, file = "cleandata/survey.csv", row.names = FALSE)
