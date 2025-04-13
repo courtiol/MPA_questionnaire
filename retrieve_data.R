9d2a20cd224e6911df3977c1ea0f7ee643158939
courtiol <alexandre.courtiol@gmail.com>
2025-04-13 11:12:05 +0200

    Add extraction details

--- retrieve_data.R
+++ retrieve_data.R
@@ -8,5 +8,16 @@ formr_connect(
 res <- formr_results(survey_name = "MPA_workforce_survey2",
                      host = "https://workforce-admin.marine-conservation.org")
 
+#saveRDS(res, file = "result_backup20250413.RDS")
+
 res |> 
-  select(where(fn = ~ !all(is.na(.x))))
+  select(where(fn = ~ !all(is.na(.x)))) |>
+  filter(!is.na(country) & (is.na(missing_MPA) | missing_MPA != "TEST"))
+
+res |> 
+  select(where(fn = ~ !all(is.na(.x)))) |>
+  filter(country == "Australia (AUS)") |>
+  mutate(across(everything(), as.character)) |>
+  pivot_longer(everything()) |>
+  filter(!is.na(value)) |>
+  as.data.frame()
