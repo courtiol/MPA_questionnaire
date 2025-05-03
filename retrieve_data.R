a82b0009bfd9d95abf09359036c74bdbe06cfac0
courtiol <alexandre.courtiol@gmail.com>
2025-05-03 17:19:55 +0200

    Only send data if they have not changed

--- retrieve_data.R
+++ retrieve_data.R
@@ -8,7 +8,7 @@ if (!interactive()) {
 }
 
 library(formr) ## remotes::install_github("rubenarslan/formr")
-library(tidyverse)
+suppressMessages(library(tidyverse, warn.conflicts = FALSE))
 
 source("secret.R") ## load credentials
 
@@ -23,37 +23,47 @@ res <- formr_results(survey_name = "MPA_workforce_survey3",
 
 #saveRDS(res, file = "responses/result_backup20250413.RDS") ## backup of pre-launch data collection
 
-nrow(res)
-sum(!is.na(res$ended))
-sum(is.na(res$country))
+hash <- digest::digest(res)
+previous_hash <- read_file("responses/lastresult_hash.txt")
 
-res |> 
-  select(where(fn = ~ !all(is.na(.x)))) |>
-  filter(!is.na(country), !missing_MPA %in% c("test", "TEST", "Test"),
-         !anythingelse %in% c("TEST"), !is.na(total_validate)) |>
-  mutate(MPA = lay::lay(pick(starts_with("MPA")), \(x) x[!is.na(x)]),
-         MPA = stringr::str_remove_all(MPA, "[:alpha:]|[:punct:]|="),
-         MPA = stringr::str_replace_all(MPA, "[:blank:]+", "-"),
-         MPA = stringr::str_remove(MPA, ".")) |>
-  select(-starts_with("MPA_")) |>
-  relocate(MPA, .after = country) -> df_clean
-
-writexl::write_xlsx(df_clean, path = paste0("./responses/clean_results_", Sys.time(), ".xlsx"))
+## do stuff only if the data have changed
+if (hash != previous_hash) {
+  write_file(hash, "responses/lastresult_hash.txt")
   
-df_clean |>
-  select(country, total_note, total_validate, starts_with("MPA"), email) |>
-  mutate(total_validate = case_match(total_validate, 1 ~ "valid",
-                                                     2 ~ "estimate",
-                                                     3 ~ "guesstimate"),
-         email = !is.na(email)) -> df_clean_short
-
-
-## send the data by email using s-nail in bash
-## I create a script called send_data_email.sh with the following
-## #!/bin/bash
-## file=$(ls responses/ -Art | tail -n 1)
-## echo $file
-## zip -e --password=[a password] responses/lastdata.zip "responses/$file"
-## echo 'Here is the latest version of the data' | mailx --subject="MPA update" -a responses/lastdata.zip [an email]
-
-if (file.exists("send_data_email.sh")) system("sh send_data_email.sh")
+  nrow(res)
+  sum(!is.na(res$ended))
+  sum(is.na(res$country))
+  
+  res |> 
+    select(where(fn = ~ !all(is.na(.x)))) |>
+    filter(!is.na(country), !missing_MPA %in% c("test", "TEST", "Test"),
+           !anythingelse %in% c("TEST"), !is.na(total_validate)) |>
+    mutate(MPA = lay::lay(pick(starts_with("MPA")), \(x) x[!is.na(x)]),
+           MPA = stringr::str_remove_all(MPA, "[:alpha:]|[:punct:]|="),
+           MPA = stringr::str_replace_all(MPA, "[:blank:]+", "-"),
+           MPA = stringr::str_remove(MPA, ".")) |>
+    select(-starts_with("MPA_")) |>
+    relocate(MPA, .after = country) -> df_clean
+  
+  writexl::write_xlsx(df_clean, path = paste0("./responses/clean_results_", Sys.time(), ".xlsx"))
+    
+  df_clean |>
+    select(country, total_note, total_validate, starts_with("MPA"), email) |>
+    mutate(total_validate = case_match(total_validate, 1 ~ "valid",
+                                                       2 ~ "estimate",
+                                                       3 ~ "guesstimate"),
+           email = !is.na(email)) -> df_clean_short
+  
+  
+  ## send the data by email using s-nail in bash
+  ## I create a script called send_data_email.sh with the following
+  ## #!/bin/bash
+  ## file=$(ls responses/ -Art | tail -n 1)
+  ## echo $file
+  ## zip -e --password=[a password] responses/lastdata.zip "responses/$file"
+  ## echo 'Here is the latest version of the data' | mailx --subject="MPA update" -a responses/lastdata.zip [an email]
+  
+  if (file.exists("send_data_email.sh")) system("sh send_data_email.sh")
+} else {
+  cat("Nothing to be done (data have not changed)\n")
+}
