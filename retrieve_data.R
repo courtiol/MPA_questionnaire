1b1b1fd340c0850cd91f4ed3c068f074720f3c54
courtiol <alexandre.courtiol@gmail.com>
2025-05-03 17:09:42 +0200

    `here::here()` does not do the job...

--- retrieve_data.R
+++ retrieve_data.R
@@ -3,7 +3,7 @@
 ## 0 * * * * Rscript [path_to_current_file]
 ## Check output using 'sudo cat /var/log/cron' (on Fedora)
 
-setwd(here::here()) ## for CRON job
+setwd("~/Dropbox/Boulot/Mes_projets_de_recherche/R_packages/MPA_project/MPA_questionnaire_Rscripts_GitHub") ## for CRON job
 
 library(formr) ## remotes::install_github("rubenarslan/formr")
 library(tidyverse)
