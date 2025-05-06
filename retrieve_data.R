8d597b455adddd8cfd77645444039a1b4c22db31
courtiol <alexandre.courtiol@gmail.com>
2025-05-06 14:42:41 +0200

    Move zip and email inside R

--- retrieve_data.R
+++ retrieve_data.R
@@ -1,11 +1,10 @@
 ## I have set this up as a CRON job to run hourly:
 ## run crontab -e and paste the following:
-## 0 * * * * Rscript [path_to_current_file]
+## 0 * * * * podman exec -ti emailMPA Rscript [path_to_current_file] ## one server
+## 0 * * * * Rscript [path_to_current_file] ## one local computer
 ## Check output using 'sudo cat /var/log/cron' (on Fedora)
 
-if (!interactive()) {
-  setwd("~/Dropbox/Boulot/Mes_projets_de_recherche/R_packages/MPA_project/MPA_questionnaire_Rscripts_GitHub") ## for CRON job
-}
+setwd("/responses") ## hard coding path is useful for running this from bash (which CRON does)
 
 library(formr) ## remotes::install_github("rubenarslan/formr")
 suppressMessages(library(tidyverse, warn.conflicts = FALSE))
@@ -14,25 +13,23 @@ source("secret.R") ## load credentials
 
 formr_connect(
   host = "https://workforce-admin.marine-conservation.org",
-  email = secret$email,
-  password = secret$password
+  email = secret$formr_login,
+  password = secret$formr_password
 )
 
 res <- formr_results(survey_name = "MPA_workforce_survey3",
                      host = "https://workforce-admin.marine-conservation.org")
 
-#saveRDS(res, file = "responses/result_backup20250413.RDS") ## backup of pre-launch data collection
-
 hash <- digest::digest(res)
-if (file.exists("responses/lastresult_hash.txt")) {
-  previous_hash <- read_file("responses/lastresult_hash.txt")
+if (file.exists("lastresult_hash.txt")) {
+  previous_hash <- read_file("lastresult_hash.txt")
 } else {
   previous_hash <- "nohash"
 }
 
 ## do stuff only if the data have changed
 if (hash != previous_hash) {
-  write_file(hash, "responses/lastresult_hash.txt")
+  write_file(hash, "lastresult_hash.txt")
   
   nrow(res)
   sum(!is.na(res$ended))
@@ -49,35 +46,29 @@ if (hash != previous_hash) {
     select(-starts_with("MPA_")) |>
     relocate(MPA, .after = country) -> df_clean
   
-  writexl::write_xlsx(df_clean, path = paste0("./responses/clean_results_", Sys.time(), ".xlsx"))
-    
-  df_clean |>
-    select(country, total_note, total_validate, starts_with("MPA"), email) |>
-    mutate(total_validate = case_match(total_validate, 1 ~ "valid",
-                                                       2 ~ "estimate",
-                                                       3 ~ "guesstimate"),
-           email = !is.na(email)) -> df_clean_short
+  last_file <- paste0("clean_results_", Sys.time(), ".xlsx")
+  writexl::write_xlsx(df_clean, path = last_file)
   
+  zip(zipfile = "lastdata", 
+      files=last_file, 
+      flags = paste("-e --password", secret$zip_password))
   
-  ## send the data by email using s-nail in bash
-  ## I create a script called prepare_zip.sh and one called send_data_email.sh as follows:
+  mailR::send.mail(from = secret$email_sender,
+                   to = secret$email_recipient,
+                   subject = "Latest data",
+                   body = "Please find enclosed the latest data.",
+                   attach.files = "lastdata.zip",
+                   smtp = list(host.name = "smtp.gmail.com",
+                               port = 587,
+                               user.name = secret$email_sender,
+                               passwd = secret$email_token, ## token set here: https://myaccount.google.com/apppasswords and call the app custom (otherwise may not work)
+                               ssl = TRUE),
+                   authenticate = TRUE,
+                   send = TRUE)
   
-  ## prepare_zip.sh
-  ## #!/bin/bash
-  ## if test -f responses/lastdata.zip; then
-  ## rm responses/lastdata.zip
-  ## fi
-  ## filexslx=
-  ## filexslx=$(ls responses/*.xlsx -Art | tail -n 1)
-  ## echo $filexslx
-  ## zip -e --password=[password] responses/lastdata.zip "$filexslx"
+  file.remove(last_file)
+  file.remove("lastdata.zip")
   
-  ## send_data_email.sh
-  ## #!/bin/bash
-  ## echo 'Here is the latest version of the data' | mailx --subject="MPA update" -a responses/lastdata.zip [email]
-  
-  if (file.exists("prepare_zip.sh")) system("sh prepare_zip.sh")
-  if (file.exists("send_data_email.sh")) system("sh send_data_email.sh")
 } else {
   cat("Nothing to be done (data have not changed)\n")
 }
